4985fc5004049a304378def50bb3ae58eae58a9a
Nathan <nathaniel.godfrey22@gmail.com>
2024-02-02 14:11:39 -0600

    added cool things and gizmos

--- app/models.py
+++ app/models.py
@@ -1,17 +1,90 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
+import base64
+import os
+import re
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
     completed = db.Column(db.Boolean, default=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
     due_date = db.Column(db.Date, nullable=True, default=None)
 
-class User:
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete_task(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # def __init__(self,**kwarfs):
+    #     allowed_fields = {'title', 'description'}
+
+    # def update(self, **kwargs):
+
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(25), nullable=False)
     password = db.Column(db.String(25), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index = True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.set_password(**kwargs.get("password", ''))
+    #     pass
+
+    # def update(self,**kwargs):
+    #     allowed_fields = {"username", "password", "email"}
+
+        # def camel_to_snake(string):
+        #     return re.sub("([A-Z][A-Za-z]*)", "_\1", string).lower()
+        # for key, value in kwargs.items():
+        #     snake_key = camel_to_snake(key)
+        #     if snake_key in allowed_fields:
+        #         if snake_key == 'password':
+        #             self.set_password(value)
+        #         else:
+        #             setattr(self, snake_key, value)
+        # self.save()
+
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+        self.get_token()
+        self.save()
+
+    def check_password(self, plain_text_password):
+        return check_password_hash(self.password, plain_text_password)
+
+    def get_token(self):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8")
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return self.token
+
+    def to_dict(self):
+        return {
+        "id": self.id,
+        "username": self.username,
+        "email": self.email,
+        "password": self.password,
+        "created": self.created
+        }
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app, db
 from datetime import datetime
 from flask import request
-from app.models import Task
+from app.models import Task, User
+from app.auths import basic_auth, token_auth
 
 
 
@@ -14,6 +15,11 @@ def index():
     last_name = 'Godfrey'
     return f'Hello World!! - From {first_name} {last_name}, now go find tasks to do!'
 
+@app.route('/token')
+@basic_auth.login_required
+def get_token():
+    current_user = basic_auth.current_user()
+    return {'token':current_user.get_token(),'token_expiration': current_user.token_expiration} 
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
@@ -48,6 +54,7 @@ def get_task(task_id):
 
 
 @app.route('/tasks', methods=['POST'])
+@token_auth.login_required
 def create_task():
     data = request.get_json()
 
@@ -63,6 +70,8 @@ def create_task():
     else:
         due_date = None
 
+    
+    
     new_task = Task(
         title=data['title'],
         description=data['description'],
@@ -72,6 +81,7 @@ def create_task():
 
     db.session.add(new_task)
     db.session.commit()
+    
 
     return {
         "id": new_task.id,
@@ -81,3 +91,82 @@ def create_task():
         "created": new_task.created,
         "due_date": due_date
     }, 201
+
+@app.route('/tasks', methods=['DELETE'])
+@token_auth.login_required
+
+def delete_task(task_id):
+    task = db.session.get(Task,task_id)
+    if task is None:
+        return {"error": "Task not found"}, 404
+
+    # Check if the current user is the owner of the task
+    if task.user_id != token_auth.current_user().id:
+        return {"error": "not authorized"}, 403
+
+
+    db.session.delete(task)
+
+    return {"message": "Task deleted successfully"}, 200
+
+# {
+#     "title":"hhff",
+#     "description":"hfhfjf"
+# }
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return {"error": "username, email and password are required fields"}, 400
+    
+
+    new_user = User(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+
+    new_user.set_password(data['password'])
+    new_user.save()
+
+    return {
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email,
+        "password": new_user.password,
+        "created": new_user.created
+        }, 201
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+@token_auth.login_required
+
+def delete_user(user_id):
+
+    user = db.session.get(User,user_id)
+    current_user = token_auth.current_user()
+    if user is None:
+        return {"error": "Task not found"}, 404
+
+    # Check if the current user is the owner of the task
+    if user != current_user:
+        return {"error": "not authorized"}, 403
+
+    user.delete_user()
+    return {"message": "user deleted successfully"}, 200
+#{ 
+# "username":"Ricardo_num_1",
+#     "email":"hdhdhd",
+#     "password":"abc123"
+# }
+
+
+@app.route('/users', methods=['DELETE'])
+@token_auth.login_required
+
+def delete_users():
+    db.session.delete(token_auth.current_user())
+
+    return {"message": "user deleted successfully"}, 200
